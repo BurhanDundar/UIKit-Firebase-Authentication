2ee85277f44b05bb0a49523d39ba06b2cd08f219
BurhanDundar <57062662+BurhanDundar@users.noreply.github.com>
2023-03-12 14:51:39 +0300

    first message

---
+++ UIKit-Firebase-Authentication/SignupViewController.swift
@@ -0,0 +1,156 @@
+//
+//  signupViewController.swift
+//  UIKit-Firebase-Authentication
+//
+//  Created by Burhan Dündar on 11.03.2023.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController, UITextFieldDelegate {
+    
+    var _txtEmail: UITextField!
+    var _txtPassword: UITextField!
+    var _emailImageView: UIImageView!
+    var _passwordImageView: UIImageView!
+    var _eyeImageView: UIImageView!
+    var _loginButton: UIButton!
+    private var isPasswordHidden: Bool = true
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .white
+        super.viewDidLoad()
+        
+        navigationItem.title = "Sign Up"
+        
+        _txtEmail = UITextField()
+        //  frame: CGRect(x: 10, y: 100, width: UIScreen.main.bounds.size.width - 20, height: 50)
+        _txtEmail.translatesAutoresizingMaskIntoConstraints = false
+        _txtEmail.backgroundColor = .yellow
+        _txtEmail.borderStyle = .line
+        _txtEmail.keyboardAppearance = .dark
+        _txtEmail.keyboardType = .default
+        _txtEmail.placeholder = "Email"
+        _txtEmail.font = UIFont.systemFont(ofSize: 15.0)
+        _txtEmail.delegate = self
+        // txtEmail.leftView = UIImageView(image: UIImage(systemName: "envelope"))
+        
+        _emailImageView = UIImageView(frame: CGRect(x: 10, y: 10, width: 30, height: 20))
+        _emailImageView.image = UIImage(systemName: "envelope")
+        _txtEmail.setupLeftImage(imageView: _emailImageView)
+        
+        view.addSubview(_txtEmail)
+                
+        _txtPassword = UITextField()
+        _txtPassword.translatesAutoresizingMaskIntoConstraints = false
+        _txtPassword.backgroundColor = .yellow
+        _txtPassword.borderStyle = .line
+        _txtPassword.keyboardAppearance = .dark
+        _txtPassword.keyboardType = .default
+        _txtPassword.placeholder = "Password"
+        _txtPassword.font = UIFont.systemFont(ofSize: 15.0)
+        _txtPassword.delegate = self
+        _txtPassword.isSecureTextEntry = true
+        // txtPassword.leftView = UIImageView(image: UIImage(systemName: "lock"))
+        
+        // This is a show-hide password action, so there is only one possibility.
+        // If you want to more flexible design, seperate the imageView part
+        // and add it customizable func
+        
+        _passwordImageView = UIImageView(frame: CGRect(x: 10, y: 10, width: 30, height: 20))
+        _passwordImageView.image = UIImage(systemName: "lock")
+        
+        _eyeImageView = UIImageView(frame: CGRect(x: 10, y: 10, width: 30, height: 20))
+        _eyeImageView.image = UIImage(systemName: "eye")
+        
+        let _tapGr = UITapGestureRecognizer(target: self, action: #selector(showHidePassword))
+        _eyeImageView.addGestureRecognizer(_tapGr)
+        _eyeImageView.isUserInteractionEnabled = true
+        
+        _txtPassword._setupLeftImage(imageView: _passwordImageView)
+        _txtPassword._setupRightImage(imageView: self._eyeImageView) // eye-eye.slash
+        
+        view.addSubview(_txtPassword)
+        
+        _loginButton = UIButton()
+        _loginButton.translatesAutoresizingMaskIntoConstraints = false
+        _loginButton.backgroundColor = .blue
+        _loginButton.addTarget(self, action: #selector(goToLoginPage), for: .touchUpInside)
+        _loginButton.setTitle("Login!", for: .normal)
+        _loginButton.setTitleColor(.white, for: .normal)
+        
+        view.addSubview(_loginButton)
+        
+        
+        NSLayoutConstraint.activate([
+            // txtEmail
+            _txtEmail.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            _txtEmail.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            _txtEmail.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
+            _txtEmail.heightAnchor.constraint(equalToConstant: 50),
+            
+            // txtPassword
+            _txtPassword.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            _txtPassword.topAnchor.constraint(equalTo: _txtEmail.bottomAnchor, constant: 50),
+            _txtPassword.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
+            _txtPassword.heightAnchor.constraint(equalToConstant: 50),
+            
+            // Sign up button
+            _loginButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            _loginButton.topAnchor.constraint(equalTo: _txtPassword.bottomAnchor, constant: 50),
+            _loginButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
+            _loginButton.heightAnchor.constraint(equalToConstant: 50)
+            
+        ])
+        
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @objc private func showHidePassword(_ sender: UITapGestureRecognizer){ // func içine imageView'ları alarak da deneyeceğim sonra
+        self.isPasswordHidden.toggle()
+        
+        if(self.isPasswordHidden){
+            self._eyeImageView.image = UIImage(systemName: "eye")
+            self._txtPassword._setupRightImage(imageView: _eyeImageView) // eye-eye.slash
+            self._txtPassword.isSecureTextEntry = true
+        } else {
+            self._eyeImageView.image = UIImage(systemName: "eye.slash")
+            self._txtPassword._setupRightImage(imageView: _eyeImageView) // eye-eye.slash
+            self._txtPassword.isSecureTextEntry = false
+        }
+    }
+    
+    @objc private func goToLoginPage(_ sender: UIButton){
+        print("go to sign in")
+        let viewController = ViewController()
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
+extension UITextField {
+    
+    func _setupRightImage(imageView: UIImageView){
+        let imageContainerView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 55, height: 40))
+        imageContainerView.addSubview(imageView)
+        rightView = imageContainerView
+        rightViewMode = .always
+        self.tintColor = .lightGray
+    }
+    
+    func _setupLeftImage(imageView: UIImageView){
+           let imageContainerView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 55, height: 40))
+           imageContainerView.addSubview(imageView)
+           leftView = imageContainerView
+           leftViewMode = .always
+           self.tintColor = .lightGray
+         }
+}
---
+++ UIKit-Firebase-Authentication/ViewController.swift
@@ -0,0 +1,155 @@
+//
+//  ViewController.swift
+//  UIKit-Firebase-Authentication
+//
+//  Created by Burhan Dündar on 11.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    var txtEmail: UITextField!
+    var txtPassword: UITextField!
+    var emailImageView: UIImageView!
+    var passwordImageView: UIImageView!
+    var eyeImageView: UIImageView!
+    var signUpButton: UIButton!
+    private var isPasswordHidden: Bool = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Login"
+        
+        txtEmail = UITextField()
+        //  frame: CGRect(x: 10, y: 100, width: UIScreen.main.bounds.size.width - 20, height: 50)
+        txtEmail.translatesAutoresizingMaskIntoConstraints = false
+        txtEmail.backgroundColor = .yellow
+        txtEmail.borderStyle = .line
+        txtEmail.keyboardAppearance = .dark
+        txtEmail.keyboardType = .default
+        txtEmail.placeholder = "Email"
+        txtEmail.font = UIFont.systemFont(ofSize: 15.0)
+        txtEmail.delegate = self
+        // txtEmail.leftView = UIImageView(image: UIImage(systemName: "envelope"))
+        
+        emailImageView = UIImageView(frame: CGRect(x: 10, y: 10, width: 30, height: 20))
+        emailImageView.image = UIImage(systemName: "envelope")
+        txtEmail.setupLeftImage(imageView: emailImageView)
+        
+        view.addSubview(txtEmail)
+                
+        txtPassword = UITextField()
+        txtPassword.translatesAutoresizingMaskIntoConstraints = false
+        txtPassword.backgroundColor = .yellow
+        txtPassword.borderStyle = .line
+        txtPassword.keyboardAppearance = .dark
+        txtPassword.keyboardType = .default
+        txtPassword.placeholder = "Password"
+        txtPassword.font = UIFont.systemFont(ofSize: 15.0)
+        txtPassword.delegate = self
+        txtPassword.isSecureTextEntry = true
+        // txtPassword.leftView = UIImageView(image: UIImage(systemName: "lock"))
+        
+        // This is a show-hide password action, so there is only one possibility.
+        // If you want to more flexible design, seperate the imageView part
+        // and add it customizable func
+        
+        passwordImageView = UIImageView(frame: CGRect(x: 10, y: 10, width: 30, height: 20))
+        passwordImageView.image = UIImage(systemName: "lock")
+        
+        eyeImageView = UIImageView(frame: CGRect(x: 10, y: 10, width: 30, height: 20))
+        eyeImageView.image = UIImage(systemName: "eye")
+        
+        let tapGr = UITapGestureRecognizer(target: self, action: #selector(showHidePassword))
+        eyeImageView.addGestureRecognizer(tapGr)
+        eyeImageView.isUserInteractionEnabled = true
+        
+        txtPassword.setupLeftImage(imageView: passwordImageView)
+        txtPassword.setupRightImage(imageView: self.eyeImageView) // eye-eye.slash
+        
+        view.addSubview(txtPassword)
+        
+        signUpButton = UIButton()
+        signUpButton.translatesAutoresizingMaskIntoConstraints = false
+        signUpButton.backgroundColor = .blue
+        signUpButton.addTarget(self, action: #selector(goToSignUpPage), for: .touchUpInside)
+        signUpButton.setTitle("Sign Up!", for: .normal)
+        signUpButton.setTitleColor(.white, for: .normal)
+        
+        view.addSubview(signUpButton)
+        
+        
+        NSLayoutConstraint.activate([
+            // txtEmail
+            txtEmail.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            txtEmail.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            txtEmail.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
+            txtEmail.heightAnchor.constraint(equalToConstant: 50),
+            
+            // txtPassword
+            txtPassword.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            txtPassword.topAnchor.constraint(equalTo: txtEmail.bottomAnchor, constant: 50),
+            txtPassword.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
+            txtPassword.heightAnchor.constraint(equalToConstant: 50),
+            
+            // Sign up button
+            signUpButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            signUpButton.topAnchor.constraint(equalTo: txtPassword.bottomAnchor, constant: 50),
+            signUpButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
+            signUpButton.heightAnchor.constraint(equalToConstant: 50)
+            
+        ])
+        
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @objc private func showHidePassword(_ sender: UITapGestureRecognizer){ // func içine imageView'ları alarak da deneyeceğim sonra
+        self.isPasswordHidden.toggle()
+        
+        if(self.isPasswordHidden){
+            self.eyeImageView.image = UIImage(systemName: "eye")
+            self.txtPassword.setupRightImage(imageView: eyeImageView) // eye-eye.slash
+            self.txtPassword.isSecureTextEntry = true
+        } else {
+            self.eyeImageView.image = UIImage(systemName: "eye.slash")
+            self.txtPassword.setupRightImage(imageView: eyeImageView) // eye-eye.slash
+            self.txtPassword.isSecureTextEntry = false
+        }
+    }
+    
+    @objc private func goToSignUpPage(_ sender: UIButton){
+        print("go to sign up page")
+        let signupViewController = SignupViewController()
+        navigationController?.pushViewController(signupViewController, animated: true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
+extension UITextField {
+    
+    func setupRightImage(imageView: UIImageView){
+        let imageContainerView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 55, height: 40))
+        imageContainerView.addSubview(imageView)
+        rightView = imageContainerView
+        rightViewMode = .always
+        self.tintColor = .lightGray
+    }
+    
+    func setupLeftImage(imageView: UIImageView){
+           let imageContainerView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 55, height: 40))
+           imageContainerView.addSubview(imageView)
+           leftView = imageContainerView
+           leftViewMode = .always
+           self.tintColor = .lightGray
+         }
+}
